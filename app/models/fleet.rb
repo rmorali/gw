80981a24bc69f3a595fd6ccf395dcf50c9241ad4
rmorali <rodrigo.morali@gmail.com>
2016-04-05 14:44:35 -0700

    Prevents carried or moving fleets group

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -54,7 +54,7 @@ class Fleet < GenericFleet
         Fleet.create(:generic_unit_id => unit.id, :planet_id => planet.id, :squad_id => squad.id, :fleet_name => '', :quantity => 1, :level => 0, :round => Round.getInstance.number)
       end
     else
-      fleet = find_or_create_by_generic_unit_id_and_planet_id_and_squad_id_and_weapon1_id_and_weapon2_id(:generic_unit_id => unit.id, :planet_id => planet.id, :squad_id => squad.id, :fleet_name => '', :weapon1_id => nil, :weapon2_id => nil, :level => 0, :round => Round.getInstance.number)
+      fleet = find_or_create_by_generic_unit_id_and_planet_id_and_squad_id_and_weapon1_id_and_weapon2_id_and_carried_by_id_and_moving(:generic_unit_id => unit.id, :planet_id => planet.id, :squad_id => squad.id, :fleet_name => '', :weapon1_id => nil, :weapon2_id => nil, :carried_by_id => nil, :moving => nil, :level => 0, :round => Round.getInstance.number)
       if fleet.quantity
         fleet.quantity += quantity
       else
