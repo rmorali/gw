73a91189761f777c78d18cf45a79e61d5e683823
rmorali <rodrigo.morali@gmail.com>
2018-01-22 09:46:33 -0800

    minor adjusts on the map

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20180108181415) do
 
-  create_table "admin_notes", :force => true do |t|
-    t.string   "resource_id",     :null => false
-    t.string   "resource_type",   :null => false
-    t.integer  "admin_user_id"
-    t.string   "admin_user_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admin_notes", ["admin_user_type", "admin_user_id"], :name => "index_admin_notes_on_admin_user_type_and_admin_user_id"
-  add_index "admin_notes", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
   create_table "generic_fleets", :force => true do |t|
     t.integer  "squad_id"
     t.integer  "planet_id"
