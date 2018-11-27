755ebc876f55525dc57c68c2800e9a02d8da66fa
rmorali <rodrigo.morali@gmail.com>
2018-11-27 09:04:40 -0800

    Edit login pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:index]
 
   def index
-    @rounds = Round.includes(:results)
-    #redirect_to "/users/sign_in"
+    #@rounds = Round.includes(:results)
+    redirect_to "/users/sign_in"
   end
 
 end
